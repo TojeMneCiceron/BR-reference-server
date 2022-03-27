9cf18590cb4b1e8885a6f9de4efefd4271336cac
TojeMneCiceron <skorobogatova.mm5910@gmail.com>
2022-03-28 03:29:46 +0500

    removed host

--- app.js
+++ app.js
@@ -133,6 +133,6 @@ const requestListener = function (req, res) {
 };
 
 const server = http.createServer(requestListener);
-server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}`);
+server.listen(port, () => {
+    console.log(`Server is running on ${port}`);
 });
